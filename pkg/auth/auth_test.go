8708ce16ab822eacc42dcb14a1390e34d35cc362
Siddharth9890 <siddharth.a9890@gmail.com>
2024-10-18 10:55:38 +0530

    test: auth

--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -46,6 +46,67 @@ func TestAuthSuite(t *testing.T) {
 		assert.Equal(t, "test-token", token)
 	})
 
+	t.Run("TestLoginEtherr", func(t *testing.T) {
+		// Reset mock
+		httpmock.Reset()
+
+		// Set up mock response
+		fixture := `{"token": "test-token"}`
+		responder := func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(200, fixture)
+			resp.Header.Set("Content-Type", "application/json")
+			return resp, nil
+		}
+		httpmock.RegisterResponder("POST", common.AuthenticateAccount, responder)
+
+		// Test
+		_, err := authImpl.Login("test", "0x1bd287faf757527cd96d0461bd3012fec227c0b85045169b3e2d4fbc8b9a2c55580db184c33a3810404aa1787151e28e647847a8dfd4d3195c64749494d18421b", "0x125b968F9ac42F33b0e1f1FBEbeE016Ca24A7116")
+
+		// Assertions
+		assert.Error(t, err)
+	})
+
+	t.Run("TestLoginSol", func(t *testing.T) {
+		// Reset mock
+		httpmock.Reset()
+
+		// Set up mock response
+		fixture := `{"token": "test-token"}`
+		responder := func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(200, fixture)
+			resp.Header.Set("Content-Type", "application/json")
+			return resp, nil
+		}
+		httpmock.RegisterResponder("POST", common.AuthenticateAccount, responder)
+
+		// Test
+		token, err := authImpl.Login("test", "4mWRM4VmpkJ4uSCAYTFjYuCZRLD5BGs7gwyrRckCGnF7HZkjdUXJRX7bBXnTpuyiWi2BYvPaTQB9QGHFw9jeTdwT", "AqzrrxaBCXRsq2BaY32djAp38B42asRRahbsYvD5uvSF")
+
+		// Assertions
+		assert.NoError(t, err)
+		assert.Equal(t, "test-token", token)
+	})
+
+	t.Run("TestLoginSolError", func(t *testing.T) {
+		// Reset mock
+		httpmock.Reset()
+
+		// Set up mock response
+		fixture := `{"token": "test-token"}`
+		responder := func(req *http.Request) (*http.Response, error) {
+			resp := httpmock.NewStringResponse(200, fixture)
+			resp.Header.Set("Content-Type", "application/json")
+			return resp, nil
+		}
+		httpmock.RegisterResponder("POST", common.AuthenticateAccount, responder)
+
+		// Test
+		_, err := authImpl.Login("test", "mWRM4VmpkJ4uSCAYTFjYuCZRLD5BGs7gwyrRckCGnF7HZkjdUXJRX7bBXnTpuyiWi2BYvPaTQB9QGHFw9jeTdwT", "AqzrrxaBCXRsq2BaY32djAp38B42asRRahbsYvD5uvSF")
+
+		// Assertions
+		assert.Error(t, err)
+	})
+
 	t.Run("TestLoginError", func(t *testing.T) {
 		// Reset mock
 		httpmock.Reset()
