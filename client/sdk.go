91ddf33d0021e28f4e980d57bbb5c1adf825620b
Siddharth9890 <siddharth.a9890@gmail.com>
2024-11-04 12:52:02 +0530

    feat: added reinitialize function

--- client/sdk.go
+++ client/sdk.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	
 	"github.com/go-resty/resty/v2"
 )
 
@@ -55,35 +56,35 @@ func NewSDK(config SDKConfig) *SDK {
 	}
 }
 
-// func (sdk *SDK) Reinitialize(config SDKConfig) {
-// 	client := resty.New()
+func (sdk *SDK) Reinitialize(config SDKConfig) *SDK {
+	client := resty.New()
 
-// 	if config.URL != "" {
-// 		client.SetBaseURL(config.URL)
-// 	} else {
-// 		client.SetBaseURL("https://dev.api.gateway.tech")
-// 	}
+	if config.URL != "" {
+		client.SetBaseURL(config.URL)
+	} else {
+		client.SetBaseURL("https://dev.api.gateway.tech")
+	}
 
-// 	if config.ApiKey != "" {
-// 		client.SetAuthToken(config.ApiKey)
-// 	} else {
-// 		wallet, _ := services.NewWalletService(config.WalletDetails.PrivateKey, config.WalletDetails.WalletType)
-// 		params := services.MiddlewareParams{
-// 			Client: client,
-// 			Wallet: *wallet,
-// 		}
-// 		client.OnBeforeRequest(helpers.AuthMiddleware(params))
-// 	}
+	if config.ApiKey != "" {
+		client.SetAuthToken(config.ApiKey)
+	} else {
+		wallet, _ := NewWalletService(config.WalletDetails.PrivateKey, config.WalletDetails.WalletType)
+		params := MiddlewareParams{
+			Client: client,
+			Wallet: *wallet,
+		}
+		client.OnBeforeRequest(AuthMiddleware(params))
+	}
 
-// 	sdkClient :=  SDKConfig{
-// 		Client: client,
-// 	}
+	sdkClient := Config{
+		Client: client,
+	}
 
-// 	return &SDK{
-// 		DataAssets: dataassets.NewDataAssetImpl(sdkClient),
-// 		DataModel:  datamodels.NewDataModelImpl(sdkClient),
-// 		Auth:       auth.NewAuthImpl(sdkClient),
-// 		ACL:        dataassets.NewACLImpl(sdkClient),
-// 		Account:    accounts.NewAccountsImpl(sdkClient),
-// 	}
-// }
+	return &SDK{
+		DataAssets: NewDataAssetImpl(sdkClient),
+		DataModel:  NewDataModelImpl(sdkClient),
+		Auth:       NewAuthImpl(sdkClient),
+		ACL:        NewACLImpl(sdkClient),
+		Account:    NewAccountsImpl(sdkClient),
+	}
+}
--- client/sdk_test.go
+++ client/sdk_test.go
@@ -40,3 +40,52 @@ func TestNewSDK_WithoutAPIKey_UseWallet(t *testing.T) {
 
 	assert.Equal(t, "https://example.com", sdk.Account.Config.Client.BaseURL, "BaseURL should be set correctly")
 }
+
+func TestSDK_Reinitialize_WithAPIKey(t *testing.T) {
+	config := client.SDKConfig{
+		ApiKey: "test-api-key",
+		URL:    "https://example.com",
+	}
+
+	sdk := client.NewSDK(config)
+	newConfig := client.SDKConfig{
+		ApiKey: "new-test-api-key",
+		URL:    "https://new-example.com",
+	}
+
+	reinitializedSDK := sdk.Reinitialize(newConfig)
+
+	assert.NotNil(t, reinitializedSDK, "Reinitialized SDK instance should not be nil")
+	assert.NotNil(t, reinitializedSDK.Auth, "Auth should not be nil")
+	assert.NotNil(t, reinitializedSDK.DataAssets, "DataAssets should not be nil")
+	assert.Equal(t, "https://new-example.com", reinitializedSDK.Account.Config.Client.BaseURL, "BaseURL should be updated correctly")
+}
+
+func TestSDK_Reinitialize_WithoutAPIKey_UseWallet(t *testing.T) {
+	initialWalletDetails := client.WalletDetails{
+		PrivateKey: "edb0ba5a63c5f9e4f4394560907794fca750704b355413bc04baab896254036a",
+		WalletType: client.Ethereum,
+	}
+	config := client.SDKConfig{
+		WalletDetails: initialWalletDetails,
+		URL:           "https://example.com",
+	}
+
+	sdk := client.NewSDK(config)
+
+	newWalletDetails := client.WalletDetails{
+		PrivateKey: "T8HMDTLmyQgY6VjvLdEwSSZsexAtiFvfiKBzEsT3ajNQg7jJgnTBK2qDSShz98ND3ihtrwrQcUWokdQr4ozPQt3",
+		WalletType: client.Solana,
+	}
+	newConfig := client.SDKConfig{
+		WalletDetails: newWalletDetails,
+		URL:           "https://new-example.com",
+	}
+
+	reinitializedSDK := sdk.Reinitialize(newConfig)
+
+	assert.NotNil(t, reinitializedSDK, "Reinitialized SDK instance should not be nil")
+	assert.NotNil(t, reinitializedSDK.Auth, "Auth should not be nil")
+	assert.NotNil(t, reinitializedSDK.DataAssets, "DataAssets should not be nil")
+	assert.Equal(t, "https://new-example.com", reinitializedSDK.Account.Config.Client.BaseURL, "BaseURL should be updated correctly")
+}
